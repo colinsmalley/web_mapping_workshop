424feae177a883b930dd9b7f9b383c417531fff1
colinsmalley <colin.c.smalley@usace.army.mil>
2014-11-13 14:52:32 -0600

    Update app_3.js

--- js/app_3.js
+++ js/app_3.js
@@ -1,41 +1,35 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
-
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
-
+var mapId = 'colinsmalley.k7c3eb9n'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoiY29saW5zbWFsbGV5IiwiYSI6Ims5eGM5V0kifQ.CS8jxClogqQG_mbUZtoUWw'; //<- this is my token, use yours.
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
 var map = L.mapbox.map('map', mapId);
-
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
-
 ///////////////////////////////////////////////////////////////////////////
 // This is the area we're going to use to add data to our map
-
-var dataFileToAdd = 'data/powercat.geojson'; //<- Point this to the file that you want to include on the map
+var dataFileToAdd = 'data/McConnell.geojson'; //<- Point this to the file that you want to include on the map
 var dataToAdd;
-
 var featureLayer = L.mapbox.featureLayer()
-    .loadURL(dataFileToAdd)
-    .addTo(map);
-
+.loadURL(dataFileToAdd)
+.addTo(map);
 featureLayer.on('ready', function() {
-    this.setStyle({
-        "color": "#43094c",
-        "fillColor": "#43094c",
-        "weight": .5,
-        "opacity": 0.65
-    });
-    map.fitBounds(featureLayer.getBounds());
+this.setStyle({
+"color": "#B28C00",
+"fillColor": "#B28C00",
+"weight": .5,
+"opacity": 0.65
 });
+map.fitBounds(featureLayer.getBounds());
+});
+
 
 ///////////////////////////////////////////////////////////////////////////
 // Add some basic click handling
 
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-    layer.bindPopup('Hi, my ID value is ' + layer.feature.properties.id);
+    layer.bindPopup('Hi, my AFB value is ' + layer.feature.properties.AFB);
   });
 });
