68e81d221bf6fe765e8a17b0ccfba68ee44e4d0f
colinsmalley <colin.c.smalley@usace.army.mil>
2014-11-13 14:29:02 -0600

    Update app_2.js

--- js/app_2.js
+++ js/app_2.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'colinsmalley.k7c3eb9n'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoiY29saW5zbWFsbGV5IiwiYSI6Ims5eGM5V0kifQ.CS8jxClogqQG_mbUZtoUWw'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
@@ -14,7 +14,7 @@ map.setView([39, -96], 4);
 ///////////////////////////////////////////////////////////////////////////
 // This is the area we're going to use to add data to our map
 
-var dataFileToAdd = 'data/powercat.geojson'; //<- Point this to the file that you want to include on the map
+var dataFileToAdd = 'data/McConnell.geojson'; //<- Point this to the file that you want to include on the map
 var dataToAdd;
 
 var featureLayer = L.mapbox.featureLayer()
